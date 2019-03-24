5b9592c308593173a8ba5192664243ba8783f0ba
Fedor Markovich <Fedor@MacBook-Air-Fedor.local>
2019-03-24 23:42:02 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+
+    let coins = {};
+const money = {
+    P: 1,
+    N: 5,
+    D: 10,
+    Q: 25,
+    H: 50,
+};
+    
+        
+    
+        if (Math.floor(currency/money.H) > 0) {
+            coins.H = Math.floor(currency/money.H);
+            currency -= coins.H*money.H;
+        }
+    
+        if (Math.floor(currency/money.Q) > 0) {
+            coins.Q = Math.floor(currency/money.Q);
+            currency -= coins.Q*money.Q;
+        }
+    
+        if (Math.floor(currency/money.D) > 0) {
+            coins.D = Math.floor(currency/money.D);
+            currency -= coins.D*money.D;
+        }
+    
+        if (Math.floor(currency/money.N) > 0) {
+            coins.N = Math.floor(currency/money.N);
+            currency -= coins.N*money.N;
+        }
+    
+        if (Math.floor(currency/money.P) > 0) {
+            coins.P = Math.floor(currency/money.P);
+        }
+    
+        return coins;
+    }
